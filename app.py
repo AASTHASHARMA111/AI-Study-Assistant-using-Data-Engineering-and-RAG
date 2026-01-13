307f4d8b259afd852059c736b6a0adf3a2ac1e39
Aastha Sharma <116420581+AASTHASHARMA111@users.noreply.github.com>
2026-01-13 21:18:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 # --- INITIALIZATION ---
 nest_asyncio.apply()
 os.environ["GOOGLE_API_KEY"] = "ENTER YOUR API KEY" #enter your api key#
+
+# 2026 Stable Models
+Settings.llm = GoogleGenAI(model="models/gemini-2.5-flash")
+Settings.embed_model = GoogleGenAIEmbedding(model_name="models/text-embedding-004")
+Settings.chunk_size = 1024
